4dd48f4b29271b27eff015348a6b31789104db08
ouisaaa_pc <ouisaaa@gmail.com>
2024-04-04 00:23:38 +0900

    0403

--- src/main/java/org/example/mapper/LocationMapper.java
+++ src/main/java/org/example/mapper/LocationMapper.java
@@ -15,10 +15,4 @@ public interface LocationMapper {
     @Select("select nx,ny from location.location where city=#{city} and district=#{district} and neighborhood=#{neighborhood}")
     LocationDao find(@Param("city") String city,@Param("district") String district,@Param("neighborhood") String neighborhood );
 
-    @Select("select district from location.location where city=#{city} order by district ASC;")
-    List<String> findToDistrict(@Param("city") String city);
-
-    @Select("select neighborhood from location.location where district=#{district} order by neighborhood ASC;")
-    List<String> findToNeighborhood(@Param("district") String district);
-
 }
--- src/main/java/org/example/service/WeatherService.java
+++ src/main/java/org/example/service/WeatherService.java
@@ -19,9 +19,6 @@ import java.util.TreeSet;
 public class WeatherService {
     private final LocationMapper locationMapper;
     private final openAPIRequest openAPIRequest;
-    public List<LocationDao> test(){
-        return locationMapper.tester();
-    }
 
     public LocationDao searchLocation(String city, String district, String neighborhood){
        return locationMapper.find(city, district, neighborhood);
@@ -31,13 +28,4 @@ public class WeatherService {
         APIResult apiResult =openAPIRequest.SrtNcst(location.getNx(), location.getNy());
         return apiResult.plusJson(apiResult.calculateWindChill());
     }
-    public TreeSet<String> searchDistrict(String city){
-        TreeSet<String> districtList = new TreeSet<>(locationMapper.findToDistrict(city)); //중복제거와 정렬을 위해 TreeSet 사용
-
-        return districtList;
-    }
-    public List<String> searchNeighborhood(String district){
-        return locationMapper.findToNeighborhood(district);
-    }
-
 }
